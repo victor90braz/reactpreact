1e659ba053c71f9d4cb96b248c7348a77cd924aa
victor90braz <victor.90braz@gmail.com>
2023-08-21 21:47:19 +0200

    Add Description for detail product

--- src/components/ProductDescription/ProductDescription.jsx
+++ src/components/ProductDescription/ProductDescription.jsx
@@ -2,14 +2,48 @@ import React from "react";
 
 const ProductDescription = ({ product }) => {
   return (
-    <div key={product.id}>
+    <div>
       <section>
         <h3>Description</h3>
-        <div>
-          <h4>{product.brand}</h4>
-          <p>Model: {product.model}</p>
-          <p>Price: {product.price}</p>
-        </div>
+        <ul>
+          <li>
+            <strong>Brand:</strong> {product.brand}
+          </li>
+          <li>
+            <strong>Model:</strong> {product.model}
+          </li>
+          <li>
+            <strong>Price:</strong> {product.price}
+          </li>
+          <li>
+            <strong>CPU:</strong> {product.cpu}
+          </li>
+          <li>
+            <strong>RAM:</strong> {product.ram}
+          </li>
+          <li>
+            <strong>Operating System:</strong> {product.os}
+          </li>
+          <li>
+            <strong>Display Resolution:</strong> {product.displayResolution}
+          </li>
+          <li>
+            <strong>Battery:</strong> {product.battery}
+          </li>
+          <li>
+            <strong>Primary Camera:</strong> {product.primaryCamera.join(", ")}
+          </li>
+          <li>
+            <strong>Secondary Camera:</strong>{" "}
+            {product.secondaryCmera.join(", ")}
+          </li>
+          <li>
+            <strong>Dimensions:</strong> {product.dimentions}
+          </li>
+          <li>
+            <strong>Weight:</strong> {product.weight}
+          </li>
+        </ul>
       </section>
     </div>
   );
